a07f781407c2abe7c367fce4754f57e5134d1b69
Mohammed@almallahi98 <x52-@hotmail.com>
2022-12-07 02:06:41 +0300

    adding zod validator & prisma db clinet was added form tha pass commit

---
+++ server/src/middelware/zod_validator/validate.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express'
+import {z, ZodError} from 'zod'
+
+const validator =(schema:z.AnyZodObject)=>(req:Request,res:Response,next:NextFunction)=>{
+    try{
+        schema.parse({
+            body:req.body,
+            params:req.params,
+            query:req.query
+        })
+    }
+    catch(err){
+        const zodError=err as ZodError;
+        return res.status(400).json({msg:{
+            error:zodError.errors[0].message,
+            code:zodError.errors[0].code,
+            path:zodError.errors[0].path
+        }})
+    }
+}
