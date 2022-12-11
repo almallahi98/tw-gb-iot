288595512f659a63bb77458862f1108f7a109c4d
Mohammed@almallahi98 <x52-@hotmail.com>
2022-12-12 02:26:38 +0300

    login clint to server is done

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,12 @@ import { connectionBD } from './context/context.db';
 import nodesRouter from './route/nodes.router';
 import sensorsRouter from './route/sensors.router';
 import userRouter from './route/user.router';
+import cros from 'cors'
 const port=5000;
 const app = exprees()
 app.use(exprees.json());
 connectionBD();
-
+app.use(cros());
 app.use('/api/v1/user',userRouter);
 app.use('/api/v1/nodes',nodesRouter);
 app.use('/api/v1/sensors',sensorsRouter);
